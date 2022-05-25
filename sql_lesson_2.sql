14fdf41e748961ac8d4d7f08ce286f6c10a6730c
ZheldakS <100212478+ZheldakS@users.noreply.github.com>
2022-05-26 01:00:23 +0600

    Add files via upload

--- sql_lesson_2.sql
+++ sql_lesson_2.sql
@@ -1,18 +1,18 @@
--- Задание 1
--- MySQL - установка +
--- my.ini в C:\Program Files\MySQL\MySQL Server 8.0
+# Задание 1
+# MySQL - установка +
+# my.ini в C:\Program Files\MySQL\MySQL Server 8.0
 
 /* Задание 2
 Создайте базу данных example, 
 разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
 */ 
--- создание новой БД
+# создание новой БД
 create database example;
--- переключение на БД
+# переключение на БД
 use example;
--- создание таблицы
+# создание таблицы
 create table users(Id INT(5) COMMENT 'ID',User CHAR(20) COMMENT 'User');
--- вывод результата
+# вывод результата
 describe users;
 
 
@@ -20,11 +20,11 @@ describe users;
 Создайте дамп базы данных example из предыдущего задания, 
 разверните содержимое дампа в новую базу данных sample.
 */ 
--- создание новой БД в которую будет копироваться example
+# создание новой БД в которую будет копироваться example
 create database sample;
 -- создание дампа в cmd
 \! mysqldump example > example.sql
--- разворот в другую БД
+# разворот в другую БД
 \! mysql sample < example.sql
 
 /* Задание 4
@@ -34,9 +34,9 @@ create database sample;
 */ 
 
 \! mysqldump mysql help_keyword --where="true limit 100" > mysql_100.sql
--- копирование в новую БД
+# копирование в новую БД
 \! mysql mysql_100 < mysql_100.sql
--- вывод таблицы
+# вывод таблицы
 describe help_keyword;
 
 /*не смог разобраться: из под администратора в командной строке windows mysqldump работает, 
